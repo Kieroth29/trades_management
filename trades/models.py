6431affc731445dbac8204d5c9fb6f9d5a2518de
Marcos Vinícius Queiroz <49909597+Kieroth29@users.noreply.github.com>
2022-02-05 15:36:29 -0300

    fixing code max length

--- trades/models.py
+++ trades/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Trade(models.Model):
-    code = models.CharField()
+    code = models.CharField(max_length=255)
     execution_datetime = models.DateTimeField(auto_now_add=True)
     execution_price = models.FloatField()
