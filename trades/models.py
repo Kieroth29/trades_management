478a71e8c68a502b349ebe658610e616ddcb66e9
Marcos Vinícius Queiroz <49909597+Kieroth29@users.noreply.github.com>
2022-02-05 15:24:25 -0300

    fixing property name

--- trades/models.py
+++ trades/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Trade(models.Model):
     code = models.CharField()
-    execution_datetime = models.DateTimeField(auto_add_now=True)
+    execution_datetime = models.DateTimeField(auto_now_add=True)
     execution_price = models.FloatField()
